b03f5f90b4e28e1081321094083e58aca3b0a793
Rushit Jain <rushit07@gmail.com>
2023-05-09 19:15:20 -0700

    initial front-end setup done

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const controller = require("./controller");
+
+const router = express.Router();
+
+// router.get("/suggestions", controller.getSuggestions);
+// router.get("/events", controller.getEvents);
+// router.get("/event", controller.getEvent);
+// router.get("/venue", controller.getVenue);
+
+module.exports = router;
